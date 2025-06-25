f8f1f507a0c617c4e5835b7c8198cce530fd1e40
ValberthAraujo <valberth10@gmail.com>
2025-06-25 10:55:33 -0300

    Update Sistema bancario DIO.py

--- Sistema bancario DIO.py
+++ Sistema bancario DIO.py
@@ -11,138 +11,141 @@ class Cliente:
     nascimento: str
     cpf: str
     endereco: str
+    extrato: str
+    cesta: int
+    limite_saque_qtd: int
+    limite_saque_valor: int
+    tarifa: int
+    saldo: int
+    numero_saques: int
+
+# --------------------------------------------- Variáveis globais ---------------------------------------------
+
+base_clientes = []
 
 # --------------------------------------------- Funções ---------------------------------------------
     
-def deposito():
+def deposito(saldo_cliente, extrato_cliente):
     
     valor = float(input("Informe o valor do depósito: "))
 
     if valor > 0:
         
-        saldo += valor
-        extrato += f"Depósito: R$ {valor:.2f}\n"
+        saldo_cliente += valor
+        extrato_cliente += f"Depósito: R$ {valor:.2f}\n"
 
     else:
         
         print("Operação falhou! O valor informado é inválido.")
 
+    return saldo_cliente, extrato_cliente
 
     
-def saque():
+def saque(saldo_cliente, extrato_cliente, numero_saques_cliente):
     
     valor = float(input("Informe o valor do saque: "))
 
-    if valor > saldo:
+    if valor > saldo_cliente:
         print("A operação falhou! Saldo insuficiente.")       
-    elif valor > limite:
+    elif valor > base_clientes[id_cliente].limite_saque_valor:
         print("Operação falhou! Limite de saque atingido.")
-    elif numero_saques >= limite_saque:
+    elif numero_saques_cliente >= base_clientes[id_cliente].limite_saque_qtd:
         print("Operação falhou! Você atingiu o limite de saques da sua conta!")
     elif valor > 0:
-        saldo -= valor
-        extrato += f"Saque: R$ {valor:.2f}\n"
-        numero_saques += 1
+        saldo_cliente -= valor
+        extrato_cliente += f"Saque: R$ {valor:.2f}\n"
+        numero_saques_cliente += 1
     else:
         print("Operação falhou! O valor informado é inválido.")
+    
+    return saldo_cliente, extrato_cliente, numero_saques_cliente
 
 
     
-def extrato():  
+def mostrar_extrato(extrato_cliente, saldo_cliente):
+    
     print("\n================ EXTRATO ================")
-    print(f"\nO plano selecionado foi: {cestas_disponiveis[opcao_cesta["Cesta Selecionada"]]}")
-    print("Não foram realizadas movimentações." if not extrato else extrato)
-    print(f"\nSaldo: R$ {(saldo - tarifa):.2f}")
+    print(f"\nO plano selecionado foi: {base_clientes[id_cliente].cesta}")
+    print("Não foram realizadas movimentações." if not extrato_cliente else extrato_cliente)
+    print(f"\nSaldo: R$ {(saldo_cliente - base_clientes[id_cliente].tarifa):.2f}")
     print("===========================================")
+
 
 def cesta_servicos():
 
-    cestas_disponiveis = ["Prata", "Ouro", "Diamante"]
     opcao_cesta = {}
-                       
-    plano = input("""
 
-    Selecione o tipo de plano desejado:
+    while True:
+        plano = input("""
+
+        Selecione o tipo de plano desejado:
     
-    [1] Prata: 3 saques, R$ 10.00.
-    [2] Ouro: 5 saques, R$ 15.00.
-    [3] Diamante: 10 saques, R$ 20.00.
+        [1] Prata: 3 saques, 2000 limite de saque , R$ 10.00.
+        [2] Ouro: 5 saques, 5000 limite de saque , R$ 15.00.
+        [3] Diamante: 10 saques, 10000 limite de saque , R$ 20.00.
     
-    => """)
+        => """)
 
-    match plano:
+        match plano:
+            case "1":
+                opcao_cesta["Cesta selecionada"] = "Prata"
+                opcao_cesta["Limite de saques quantidade"] = 3
+                opcao_cesta["Limite de saques valor"] = 2000
+                opcao_cesta["Tarifa"] = 10
+                return opcao_cesta
+            case "2":
+                opcao_cesta["Cesta selecionada"] = "Ouro"
+                opcao_cesta["Limite de saques quantidade"] = 5
+                opcao_cesta["Limite de saques valor"] = 5000
+                opcao_cesta["Tarifa"] = 15
+                return opcao_cesta
+            case "3":
+                opcao_cesta["Cesta selecionada"] = "Diamante"
+                opcao_cesta["Limite de saques quantidade"] = 10
+                opcao_cesta["Limite de saques valor"] = 10000
+                opcao_cesta["Tarifa"] = 20
+                return opcao_cesta
+            case _:
+                print("Opção inválida. Por favor, selecione 1, 2 ou 3.")
 
-        case "1":
-            limite_saque = 3
-            tarifa = 10
-            cesta_selecionada = 0
-            if numero_saques >= limite_saque:
-                print("Plano excedido, escolha outro plano")
-            else:
-                opcao_cesta.setdefault("Limite de saques", limite_saque)
-                opcao_cesta.setdefault("Tarifa", tarifa)
-                opcao_cesta.setdefault("Cesta selecionada", cesta_selecionada)
-            exit()
-        case "2":
-            limite_saque = 5
-            tarifa = 15
-            cesta_selecionada = 1
-            if numero_saques >= limite_saque:
-                print("Plano excedido, escolha outro plano")
-            else:
-                opcao_cesta.setdefault("Limite de saques", limite_saque)
-                opcao_cesta.setdefault("Tarifa", tarifa)
-                opcao_cesta.setdefault("Cesta selecionada", cesta_selecionada)
-            exit()
-        case "3":
-            limite_saque = 10
-            tarifa = 20
-            cesta_selecionada = 2
-            if numero_saques >= limite_saque:
-                print("Plano excedido, escolha outro plano")
-            else:
-                opcao_cesta.setdefault("Limite de saques", limite_saque)
-                opcao_cesta.setdefault("Tarifa", tarifa)
-                opcao_cesta.setdefault("Cesta selecionada", cesta_selecionada)
-            exit()
-        case _:
-            print("Por favor, selecionar um dos números")
-            exit()
 
 def cadastrar_cliente():
+    
+    opcao_cesta = cesta_servicos()
+
+    cesta_selecionada = opcao_cesta["Cesta selecionada"]
+    limite_saque_qtd = opcao_cesta["Limite de saques quantidade"]
+    limite_saque_valor = opcao_cesta["Limite de saques valor"]
+    tarifa_selecionada = opcao_cesta["Tarifa"]
+
     return Cliente(
         nome = input("Insira seu nome: ").strip(),
         senha = input("Informe a senha desejada: ").strip(),
         nascimento = input("Insira sua data de nascimento: ").strip(),
         cpf = input("Informe o seu CPF: ").strip(),
-        endereco = input("Informe seu endereço: ").strip()
+        endereco = input("Informe seu endereço: ").strip(),
+        cesta = cesta_selecionada,
+        limite_saque_qtd = limite_saque_qtd,
+        limite_saque_valor = limite_saque_valor,
+        tarifa = tarifa_selecionada,
+        saldo = 0,
+        numero_saques = 0,
+        extrato = ""
     )
     
 
 def login_cliente():
     
-    cpf = input("Bem vindo cliente, insira seu cpf ").strip()
-    senha = input("Insira sua senha!").strip()
-    
+    cpf = input("Bem vindo cliente, insira seu cpf: ").strip()
+    senha = input("Insira sua senha: ").strip()
+
     for i in base_clientes:
         if i.cpf == cpf and i.senha == senha:
-            return True
-        else:
-            return False
+            return True, base_clientes.index(i)
 
-# --------------------------------------------- Variáveis globais ---------------------------------------------
+    return False, None
 
-plano = "1"
-limite_saque = 3
-tarifa = 10
-
-saldo = 0
-limite = 500
-
-extrato = ""
-base_clientes = []
-
-# --------------------------------------------- Código Principal ---------------------------------------------
+# --------------------------------------------- Programa Principal ---------------------------------------------
 
 while True:
 
@@ -152,33 +155,42 @@ while True:
     
     [1] Entrar
     [2] Cadastrar-se
-    [3] Sair
+    [0] Sair
     
     => """)
     
     if menu == "1":
-        if login_cliente() == True:
+        
+        resultado_login, id_cliente = login_cliente()
+        
+        while resultado_login:
             
-            opcao = input("""
+            opcao = input(f"""
 
-            Bem vindo ! Escolha o que deseja fazer.
+            Bem vindo {base_clientes[id_cliente].nome}! Escolha o que deseja fazer.
             
             [1] Depositar
             [2] Sacar
             [3] Extrato
             [4] Alterar plano
             [0] Sair
-            
+
             => """)
             
             if opcao == "1":
-                deposito()
+                base_clientes[id_cliente].saldo, base_clientes[id_cliente].extrato = deposito(base_clientes[id_cliente].saldo, base_clientes[id_cliente].extrato)
             elif opcao == "2":
-                saque()
+                base_clientes[id_cliente].saldo, base_clientes[id_cliente].extrato, base_clientes[id_cliente].numero_saques = saque(base_clientes[id_cliente].saldo, base_clientes[id_cliente].extrato, base_clientes[id_cliente].numero_saques)
             elif opcao == "3":
-                extrato()
+                mostrar_extrato(base_clientes[id_cliente].extrato, base_clientes[id_cliente].saldo)
             elif opcao == "4":
-                cesta_servicos()
+                dados_cesta = cesta_servicos()
+
+                base_clientes[id_cliente].cesta = dados_cesta["Cesta selecionada"]
+                base_clientes[id_cliente].limite_saque_qtd = dados_cesta["Limite de saques quantidade"]
+                base_clientes[id_cliente].limite_saque_valor = dados_cesta["Limite de saques valor"]
+                base_clientes[id_cliente].tarifa = dados_cesta["Tarifa"]
+
             elif opcao == "0":
                 exit()
             else:
@@ -191,5 +203,5 @@ while True:
         cliente = cadastrar_cliente()
         base_clientes.append(cliente)
         
-    if menu == "3":
+    if menu == "0":
         exit()
