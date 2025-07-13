d2b49a94902cd4caf2001135a1cc777726b2eb38
ValberthAraujo <valberth10@gmail.com>
2025-07-12 22:11:52 -0300

    Código revisado pelo flake8, black e isort, além da adição da pasta env para as dependencias do código.

--- Sistema bancario DIO.py
+++ Sistema bancario DIO.py
@@ -1,13 +1,8 @@
-
-# ------------------------------------------  IMPORTAÇÕES  ------------------------------------------- #
-
-from dataclasses import dataclass, field
-from typing import List
-from datetime import datetime
 from collections import defaultdict
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import List
 
-
-# --------------------------------------------  CLASSES  --------------------------------------------- #
 
 @dataclass
 class Cliente:
@@ -20,7 +15,7 @@ class Cliente:
     @staticmethod
     def cadastrar_cliente(cpf_usuario):
 
-        data_nascimento = input("Digite o data de nascimento (DDMMYYYY)")
+        data_nascimento = input("Digite a data de nascimento (DDMMYYYY)")
 
         if len(data_nascimento) != 8:
             print("Digite corretamente a data de nascimento")
@@ -42,16 +37,14 @@ class Cliente:
 
             endereco_usuario = f"{logradouro}, {bairro} - {cidade}/{estado}"
 
-            resposta = input(f"""
-
-                O endereço: 
-                {endereco_usuario}
-                está correto?
-
-                [1] Sim
-                [2] Não
-
-                => """)
+            resposta = input(
+                "O endereço:\n"
+                f"{endereco_usuario}\n"
+                "está correto?\n\n"
+                "[1] Sim\n"
+                "[2] Não\n\n"
+                "=> "
+            )
 
             if resposta == "1":
                 break
@@ -63,13 +56,14 @@ class Cliente:
             nome=input("Insira seu nome: ").strip(),
             senha=input("Informe a senha desejada: ").strip(),
             nascimento=data_nascimento,
-            endereco=endereco_usuario
+            endereco=endereco_usuario,
         )
 
     @staticmethod
     def novo_cliente():
 
-        cpf_usuario = input("Insira o seu CPF (apenas números, 11 digitos) ")
+        cpf_usuario = input("Insira o seu CPF "
+                            "(apenas números, 11 digitos) ")
 
         if len(cpf_usuario) != 11:
             print("Digite o cpf corretamente")
@@ -82,19 +76,22 @@ class Cliente:
 
     @staticmethod
     def login_cliente():
-        cpf = int(input("Bem vindo cliente, insira seu cpf (apenas números): ").strip())
+        cpf = int(input("Bem vindo cliente, "
+                        "insira seu cpf (apenas números): ").strip())
         senha = input("Insira sua senha: ").strip()
 
         for i, cliente_atual in enumerate(base_clientes):
             if cliente_atual.cpf == cpf and cliente_atual.senha == senha:
-                conta_selecionada = input(f"""
-
-                Selecione uma conta: 
-                {Conta.listar_contas(cpf)}
-
-                => """)
-                return True, base_clientes.index(cliente_atual), conta_selecionada
+                conta_selecionada = input(
+                                    "Selecione uma conta: \n"
+                                    f"{Conta.listar_contas(cpf)}\n\n"
+                                    "=>"
+                                    )
+                return (True,
+                        base_clientes.index(cliente_atual),
+                        conta_selecionada)
         return None
+
 
 @dataclass
 class Conta:
@@ -109,7 +106,6 @@ class Conta:
     agencia: str
     extrato: List[str] = field(default_factory=list)
 
-
     @staticmethod
     def deposito(saldo_cliente, extrato_cliente: list):
 
@@ -120,14 +116,12 @@ class Conta:
         valor = int(input("informe o valor do depósito"))
 
         if valor > 0:
-
             saldo_cliente += valor
-            extrato_cliente.append({"Deposito": valor, "Data": data, "Hora": hora})
-
+            extrato_cliente.append(
+                {"Deposito": valor, "Data": data, "Hora": hora}
+            )
         else:
-
             print("Operação falhou! O valor informado é inválido.")
-
         return saldo_cliente, extrato_cliente
 
     @staticmethod
@@ -140,35 +134,42 @@ class Conta:
         valor = float(input("Informe o valor do saque: "))
 
         if valor > saldo_cliente:
-            print("A operação falhou! Saldo insuficiente.")
+            print("A operação falhou!"
+                  " Saldo insuficiente.")
         elif valor > conta_acessada.limite_saque_valor:
-            print("Operação falhou! Limite de saque atingido.")
+            print("Operação falhou!"
+                  " Limite de saque atingido.")
         elif numero_saques_cliente >= conta_acessada.limite_saque_qtd:
-            print("Operação falhou! Você atingiu o limite de saques da sua conta!")
+            print("Operação falhou!"
+                  " Você atingiu o limite de saques da sua conta!")
         elif valor > 0:
             saldo_cliente -= valor
-            extrato_cliente.append({"Saque": valor, "Data": data, "Hora": hora})
+            extrato_cliente.append(
+                {"Saque": valor, "Data": data, "Hora": hora}
+            )
             numero_saques_cliente += 1
         else:
-            print("Operação falhou! O valor informado é inválido.")
+            print("Operação falhou!"
+                  " O valor informado é inválido.")
 
         return saldo_cliente, extrato_cliente, numero_saques_cliente
-    
+
     @staticmethod
     def alterar_cesta():
 
         opcao_cesta = {}
 
         while True:
-            plano = input("""
-
-            Selecione o tipo de plano desejado:
-
-            [1] Prata: 3 saques, 2000 limite de saque , R$ 10.00.
-            [2] Ouro: 5 saques, 5000 limite de saque , R$ 15.00.
-            [3] Diamante: 10 saques, 10000 limite de saque , R$ 20.00.
-
-            => """)
+            plano = input(
+                "Selecione o tipo de plano desejado: \n\n"
+                "[1] Prata:"
+                " 3 saques, 2000 limite de saque, R$ 10.00.\n"
+                "[2] Ouro:"
+                " 5 saques, 5000 limite de saque, R$ 15.00.\n"
+                "[3] Diamante:"
+                " 10 saques, 10000 limite de saque, R$ 20.00.\n\n"
+                "=>"
+            )
 
             match plano:
                 case "1":
@@ -202,25 +203,36 @@ class Conta:
             natureza = lancamento.get("Deposito")
 
             if natureza is None:
-                saida += f"Horário: {lancamento['Data']} | {lancamento['Hora']} |   Saque    | Valor: {lancamento['Saque']}\n"
+                saida += (f"Horário: {lancamento['Data']} |"
+                          f" {lancamento['Hora']} |"
+                          f"   Saque    |"
+                          f" Valor: {lancamento['Saque']}\n")
             else:
-                saida += f"Horário: {lancamento['Data']} | {lancamento['Hora']} |  Deposito  | Valor: {lancamento['Deposito']}\n"
+                saida += (f"Horário: {lancamento['Data']} |"
+                          f" {lancamento['Hora']} |"
+                          f"  Deposito  |"
+                          f" Valor: {lancamento['Deposito']}\n")
 
         return saida
-    
+
     @staticmethod
     def mostrar_extrato(extrato_cliente, saldo_cliente):
 
         extrato_cliente = Conta.formatar_extrato(extrato_cliente)
-
         print("\n================ EXTRATO ================")
-        print(f"\nSeu extrato está disponível, {base_clientes[id_cliente].nome}, confira suas informações!")
+        print("\nSeu extrato está disponível, "
+              f"{base_clientes[id_cliente].nome},"
+              f" confira suas informações!")
         print(f"\nO plano selecionado foi: {conta_acessada.cesta}")
         print(f"\nDébito cesta de serviços R$ {conta_acessada.tarifa}")
-        print("Não foram realizadas movimentações." if not extrato_cliente else extrato_cliente)
+        print(
+            "Não foram realizadas movimentações."
+            if not extrato_cliente
+            else extrato_cliente
+        )
         print(f"\nSaldo: R$ {(saldo_cliente - conta_acessada.tarifa):.2f}")
         print("===========================================")
-        
+
     @staticmethod
     def listar_contas(cpf):
         mostrar_contas = ""
@@ -228,7 +240,8 @@ class Conta:
         for i in range(0, len(cliente_contas[cpf])):
             ag = cliente_contas[cpf][i].agencia
             cc = cliente_contas[cpf][i].conta
-            mostrar_contas += f"\n{[i + 1]} Agência: {ag}, Conta corrente: {cc}"
+            mostrar_contas += (f"\n{[i + 1]} Agência: {ag},"
+                               f" Conta corrente: {cc}")
 
         return mostrar_contas
 
@@ -259,27 +272,20 @@ class Conta:
             numero_saques=0,
             extrato=[],
             agencia="0001",
-            conta=conta_usuario
+            conta=conta_usuario,
         )
 
-
-# ---------------------------------------  VARIÁVEIS GLOBAIS  ---------------------------------------- #
 
 base_clientes = []
 cliente_contas = defaultdict(list)
 
-# ---------------------------------------  PROGRAMA PRINCIPAL  --------------------------------------- #
-
 while True:
 
     menu = input("""
-             
     Escolha a opção desejada:
-    
     [1] Entrar
     [2] Cadastrar-se
     [0] Sair
-    
     => """)
 
     if menu == "1":
@@ -290,45 +296,66 @@ while True:
             print("CPF ou senha incorretos, tente novamente.")
             continue
 
-        conta_acessada = cliente_contas[base_clientes[id_cliente].cpf][int(conta_usada) - 1]
+        conta_acessada = cliente_contas[base_clientes[id_cliente].cpf][
+            int(conta_usada) - 1
+        ]
 
         while resultado_login:
 
-            opcao = input(f"""
-
-            Bem vindo {base_clientes[id_cliente].nome}, conta: {conta_acessada.conta}! Escolha o que deseja fazer.
-            
-            [1] Depositar
-            [2] Sacar
-            [3] Extrato
-            [4] Alterar plano
-            [5] Criar nova conta
-            [6] Alterar conta
-            [0] Sair
-
-            => """)
+            opcao = input(
+                f"Bem vindo {base_clientes[id_cliente].nome}\n"
+                f"conta: {conta_acessada.conta}!\n"
+                "Escolha o que deseja fazer:\n\n"
+                "[1] Depositar\n"
+                "[2] Sacar\n"
+                "[3] Extrato\n"
+                "[4] Alterar plano\n"
+                "[5] Criar nova conta\n"
+                "[6] Alterar conta\n"
+                "[0] Sair\n\n"
+                "=>"
+            )
 
             if opcao == "1":
-                conta_acessada.saldo, conta_acessada.extrato = Conta.deposito(conta_acessada.saldo, conta_acessada.extrato)
+                conta_acessada.saldo, conta_acessada.extrato = Conta.deposito(
+                    conta_acessada.saldo, conta_acessada.extrato
+                )
             elif opcao == "2":
-                conta_acessada.saldo, conta_acessada.extrato, conta_acessada.numero_saques = Conta.saque(conta_acessada.saldo, conta_acessada.extrato, conta_acessada.numero_saques)
+                (
+                    conta_acessada.saldo,
+                    conta_acessada.extrato,
+                    conta_acessada.numero_saques,
+                ) = Conta.saque(
+                    conta_acessada.saldo,
+                    conta_acessada.extrato,
+                    conta_acessada.numero_saques,
+                )
             elif opcao == "3":
-                Conta.mostrar_extrato(conta_acessada.extrato, conta_acessada.saldo)
+                Conta.mostrar_extrato(
+                    conta_acessada.extrato, conta_acessada.saldo
+                )
             elif opcao == "4":
                 dados_cesta = Conta.alterar_cesta()
             elif opcao == "5":
-                nova_conta = Conta.cadastrar_conta(base_clientes[id_cliente].cpf)
-                cliente_contas[base_clientes[id_cliente].cpf].append(nova_conta)
-                print(f"Conta criada, Agencia: {nova_conta.agencia} Conta: {nova_conta.conta}")
+                nova_conta = Conta.cadastrar_conta(
+                    base_clientes[id_cliente].cpf
+                )
+                cliente_contas[
+                    base_clientes[id_cliente].cpf
+                ].append(nova_conta)
+                print(
+                    f"Conta criada, Agencia: {nova_conta.agencia}"
+                    f" Conta: {nova_conta.conta}"
+                )
 
             elif opcao == "6":
-                conta_usada = input(f"""
-
-                                    Selecione uma conta: 
-                                    
-                                    {Conta.listar_contas(base_clientes[id_cliente].cpf)}
-                                    
-                                    => """)
+                conta_usada = input(
+                                "Selecione uma conta:\n"
+                                f"{Conta.listar_contas(
+                                    base_clientes[id_cliente].cpf
+                                )}\n"
+                                "=>"
+                              )
             elif opcao == "0":
                 exit()
             else:
